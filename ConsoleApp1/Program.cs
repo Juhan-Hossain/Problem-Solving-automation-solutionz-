43550debf4d2ad7a02aa6169d676c1a6ee58b9a2
Juhan <juhan.hossain@brainstation-23.com>
2025-02-16 21:46:49 +0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using System.IO;
+
+Console.WriteLine("Hello, World!");
+var solution = new Solution();
+var test = "aabbccz";
+
+
+//Optimized one for charstream
+// streamtext file will be uploaded
+var finder = new StreamCharacterFinder();
+
+using (var stream = new StreamReader("streamtext.txt"))
+{
+    int nextChar;
+	while ((nextChar = stream.Read())!=-1)
+	{
+		char c = (char)nextChar;
+		finder.processNextChar(c);
+
+		char? currentnonrepeating = finder.GetFirstNonRepeating();
+		if (currentnonrepeating.HasValue)
+			Console.WriteLine($"Current first non repeating: {currentnonrepeating}");
+	}
+
+}
---
+++ ConsoleApp1/Solution.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        //non optimized solution
+        public static char? FindFirstNonRepeatingChar(string str)
+        {
+            Dictionary<char, int> charCount = new();
+            foreach (char charecter in str)
+            {
+                if (charCount.ContainsKey(charecter)) charCount[charecter]++;
+                else charCount[charecter] = 1;
+            }
+
+            foreach (char charecter in str)
+            {
+                if (charCount[charecter] == 1) return charecter;
+            }
+            return null;
+        }
+
+        //Followup question:
+        private const int CHAR_SIZE = 256;
+        public static char? FindFirstNonRepeatingCharOptimizedSolution(string str)
+        {
+            //edge case analysis:
+            //
+            if(string.IsNullOrEmpty(str)) return null;
+            if(str.Length == 1) return str[0];
+
+            Span<int> charCount = stackalloc int[CHAR_SIZE];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                charCount[str[i]]++;
+
+                if (i > CHAR_SIZE && !charCount.Contains(1))
+                    return null;
+            }
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (charCount[str[i]] == 1) return str[i];
+            }
+            return null;
+        }
+        // For stream of chars:
+
+
+    }
+
+    public class StreamCharacterFinder
+    {
+        private const int CHAR_SIZE = 256;
+        private readonly int[] charCount;
+        private readonly LinkedList<char> nonRepeatingChars;
+
+        public StreamCharacterFinder()
+        {
+            charCount = new int[CHAR_SIZE];
+            nonRepeatingChars = new LinkedList<char>();
+        }
+
+        public void processNextChar(char c)
+        {
+            charCount[c]++;
+            if (charCount[c] == 1) nonRepeatingChars.AddLast(c);
+            else nonRepeatingChars.Remove(c);
+        }
+
+        public char? GetFirstNonRepeating()
+        {
+            if (nonRepeatingChars.Count > 0) return nonRepeatingChars.First.Value;
+            return null;
+        }
+        public void Reset()
+        {
+            Array.Clear(charCount, 0, CHAR_SIZE);
+            nonRepeatingChars.Clear();
+        }
+    }
+}
+
